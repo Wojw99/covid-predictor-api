cf982a77379c135d37ce4c7d092b56238aef5469
Wojw99 <42806302+Wojw99@users.noreply.github.com>
2021-04-07 13:29:41 +0200

    get all predictions

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 const express = require('express');
 const app = express();
+const csv = require('csv-parser');
+const fs = require('fs');
 
+
+// - - - - - GET main route - - - - - */
 app.get('/', function (request, response) {
     response.send('Hello World!');
 });
 
+// - - - - - GET for all predictions - - - - - */
+app.get('/api/predictions', function (request, response) {
+    var predictions = []
+
+    fs.createReadStream('data/data_predicted.csv')
+    .pipe(csv())
+    .on('open', () => {
+        console.log('start reading');
+    })
+    .on('data', (row) => {
+        predictions.push(row);
+    })
+    .on('end', () => {
+        console.log('end reading');
+        response.send(predictions);
+    });    
+});
+
+// - - - - - - - RUN SERVER - - - - -  - - 
 app.listen(3000, function(){
     console.log('Listening on port 3000...');
 });
--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 const EventEmitter = require('events');
 const Logger = require('./logger.js');
+const fs = require('fs');
 
 const logger = new Logger();
 
@@ -8,4 +9,24 @@ logger.on('messageLogged', function(args){
     console.log(`Hello Word!, ${args.id}, ${args.url}`);
 });
 
-logger.log('message');
+// logger.log('message');
+
+
+// - - - - - CSV - - - - - 
+var row = {
+    'Country/Region': 'Tajikistan ',
+    Lat: '38.861034000000004',
+    Long: '71.276093',
+    '2/10/20': '0',
+    '2/11/20': '0',
+    '2/12/20': '0',
+    '2/13/20': '0',
+    '2/14/20': '0',
+    '2/15/20': '0'
+};
+
+console.log(row.Lat);
+console.log(row['Country/Region']);
+row.forEach(element => {
+    console.log(element);
+});
